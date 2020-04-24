a7ce23709353d429a35deb7bb3ea273353f3c9bb
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-04-24 15:57:44 -0400

    added theme

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/* GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score */
-
-// for (var i = 0; i < 0; i++);
-
-
 var scores = JSON.parse(localStorage.getItem("item"));
 if (scores === null) {
     var scores = [
@@ -21,42 +6,12 @@ if (scores === null) {
 
 console.log(scores + " beg output of scores");
 
-// var scores = {
-//     name: "ddd",
-//     score: 0
-// };
-
 
 console.log(scores, " this is the user after parse");
-//create object to store name and pw
 
-
-
-
-
-// var name = nameEl.value;
-// var li = document.createElement("li");
-// li.id = people.length;
-// li.innerHTML = name + " <button>edit</button>";
-// peopleListEl.append(li);
-
-// console.log(scores);
-
-// ...
-// var tempScore = [];
-// for (var index = 0; index < tempScore.length; index++) {
-//     if (data[index].Status == "Valid") {
-//         tempData.push(data);
-//     }
-// }
-// data = tempData;
-
-
-
-var startQuizElement = document.querySelector(".start-btn");
+var startQuizElement = document.querySelector("#start-btn");
 var timeElement = document.querySelector(".time");
 var questionContainerElement = document.querySelector(".questions");
-
 var answerElement0 = document.querySelector("#answer0");
 var answerElement1 = document.querySelector("#answer1");
 var answerElement2 = document.querySelector("#answer2");
@@ -66,104 +21,111 @@ var submitForm = document.querySelector(".submit");
 var originalContent = document.querySelector(".original");
 var submitFormHighScoreBtn = document.querySelector("#submitHighScrBtn");
 var highScoresPage = document.querySelector("#highscores");
-
-highScoresPage.hidden = true;
-submitForm.hidden = true;
-
+var clearHighScoresbtn = document.querySelector("#clearHighScoresbtn");
+var hideScorePara = document.querySelector(".hideScorePara");
+var viewHighScoresbtn = document.querySelector("#viewHighScoresbtn");
+var gobackbt = document.querySelector("#goBack");
 var answerElement = document.querySelector(".btn");
-
 var numberOfQuestion = 0;
 var score = 0;
 var answerElementPicked = 0;
-
 var question1Element = document.querySelector(".question-one");
 var wrongOrWriteDisplay = document.querySelector(".right-or-wrong-msg");
-
+highScoresPage.hidden = true;
+submitForm.hidden = true;
 document.querySelector(".questions").hidden = true;
-
 document.querySelector(".right-or-wrong-msg").hidden = true;
-var timer = 5;
-
+var timer = 90;
 // var personObj = new personHighScoreObj("user", "score");
 // console.log(personObj + " this is person object");
-
-
 var answerarrayq = [
     {
-        question: "How old is dex?",
+        question: "Ned Stark had a sister who was abducted by the mad king. What was her name?",
         answers: [
-            { text: "5", iscorrect: false },
-            { text: "2", iscorrect: true },
-            { text: "3", iscorrect: false },
-            { text: "1", iscorrect: false }
+            { text: "Lysa", iscorrect: false },
+            { text: "Lorena", iscorrect: false },
+            { text: "Leanna", iscorrect: false },
+            { text: "Lyanna", iscorrect: true }
         ]
     },
     {
-        question: "How much does he weigh?",
+        question: "What is the name of Arya's Direwolf",
         answers: [
-            { text: "22", iscorrect: false },
-            { text: "21", iscorrect: false },
-            { text: "31", iscorrect: true },
-            { text: "41", iscorrect: false }
+            { text: "Grey Wind", iscorrect: false },
+            { text: "Nightfall", iscorrect: false },
+            { text: "Nymeria", iscorrect: true },
+            { text: "Lady", iscorrect: false }
         ]
     },
     {
-        question: "what is 2 + 2",
+        question: "What is Ser Davos's nickname? ",
         answers: [
-            { text: "1", iscorrect: false },
-            { text: "2", iscorrect: false },
-            { text: "3", iscorrect: false },
-            { text: "4", iscorrect: true }
+            { text: "The Onion Knight", iscorrect: true },
+            { text: "Blackfish", iscorrect: false },
+            { text: "Little Figer", iscorrect: false },
+            { text: "Halfblood", iscorrect: false }
         ]
     },
     {
-        question: "what is 3 + 3",
+        question: "Who Created the plot to kill king Joffery?",
         answers: [
-            { text: "22", iscorrect: false },
-            { text: "21", iscorrect: false },
-            { text: "31", iscorrect: true },
-            { text: "41", iscorrect: false }
+            { text: "Littlefinger", iscorrect: false },
+            { text: "tyrion", iscorrect: false },
+            { text: "Sansa", iscorrect: false },
+            { text: "Maergery", iscorrect: true }
         ]
     },
     {
-        question: "How much does he weigh?",
+        question: "Wilfire is incredibly explosive, what substance can extinguish it?",
         answers: [
-            { text: "22", iscorrect: false },
-            { text: "21", iscorrect: false },
-            { text: "31", iscorrect: true },
-            { text: "41", iscorrect: false }
+            { text: "Snow", iscorrect: false },
+            { text: "Sand", iscorrect: true },
+            { text: "Dragon's Blood", iscorrect: false },
+            { text: "Ice", iscorrect: false }
+        ]
+    },
+    {
+        question: "What two substances can kill white walkers?",
+        answers: [
+            { text: "Dragon Breath and Wildfire", iscorrect: false },
+            { text: "Valyrian Steel and Dragonglass", iscorrect: true },
+            { text: "Valyrian Steel and dragon breath", iscorrect: false },
+            { text: "Wilfire and Dragonglass", iscorrect: false }
+        ]
+    },
+    {
+        question: "Who created the Nightking",
+        answers: [
+            { text: "The Wildings", iscorrect: false },
+            { text: "The Children of the Forest", iscorrect: true },
+            { text: "The Mad King", iscorrect: false },
+            { text: "The Red lady", iscorrect: false }
         ]
     },
 ]
 
-
+var answerarrayqLength = answerarrayq.length;
 // var highScore = new Object(name, score);
 // console.log(highScore + " high score object")
-
-console.log(answerarrayq);
+// console.log(answerarrayq);
 // console.log(answerarrayq1.answers[1]);
-console.log(answerarrayq[0].answers[0].text)
-console.log(answerarrayq[0].answers[0].iscorrect)
+// console.log(answerarrayq[0].answers[0].text)
+// console.log(answerarrayq[0].answers[0].iscorrect)
 // console.log(answerarrayq.numberOfQuestion.answers.length);
-
-
-
-
 startQuizElement.addEventListener("click", function () {
-    document.querySelector(".start-btn").hidden = true;
+    highScoresPage.hidden = true;
+    document.querySelector("#start-btn").hidden = true;
     document.querySelector(".questions").hidden = false;
     document.querySelector(".title").hidden = true;
-
+    gobackbt.hidden = true;
     // startQuiz();
     time();
     var numberOfQuestion = question();
     console.log(numberOfQuestion + "number of quest");
     // return whichNumberOfQuestion
-
 });
 
 function question() {
-
     setTimeout(function () {
         document.querySelector(".right-or-wrong-msg").hidden = true;
     }, 2000);
@@ -177,7 +139,6 @@ function question() {
     answerElement2.textContent = answerarrayq[numberOfQuestion].answers[2].text;
     answerElement3.textContent = answerarrayq[numberOfQuestion].answers[3].text;
     // return [numberOfQuestion(), answerarrayq1.iscorrect()];
-
     return numberOfQuestion;
 }
 
@@ -187,37 +148,35 @@ answerElement0.addEventListener("click", function () {
     answerElementPicked = 0;
     checkIfTrue();
     question();
-
 });
 
 answerElement1.addEventListener("click", function () {
     answerElementPicked = 1;
     checkIfTrue();
     question();
-
 });
 
 answerElement2.addEventListener("click", function () {
     answerElementPicked = 2;
     checkIfTrue();
     question();
-
 });
 
 answerElement3.addEventListener("click", function () {
     answerElementPicked = 3;
     checkIfTrue();
     question();
-
 });
-
-
 submitFormHighScoreBtn.addEventListener("click", function () {
     highScores();
 
 });
-
-
+clearHighScoresbtn.addEventListener("click", function () {
+    clearHighScores();
+});
+viewHighScoresbtn.addEventListener("click", function () {
+    showHighScore();
+});
 function checkIfTrue() {
     console.log(answerarrayq[numberOfQuestion].answers[answerElementPicked].iscorrect);
     if (answerarrayq[numberOfQuestion].answers[answerElementPicked].iscorrect === true) {
@@ -236,19 +195,17 @@ function checkIfTrue() {
         document.querySelector(".right-or-wrong-msg").hidden = false;
         timer -= 10;
     }
+    answerarrayqLength--;
 }
-
-
 function time() {
     var timerInterval = setInterval(function () {
         timer--;
         timeElement.textContent = timer;
-        if (timer <= 0) {
+        if (timer <= 0 || answerarrayqLength <= 0) {
             clearInterval(timerInterval);
             gameOver();
             // alert("time is up");
         }
-
     }, 1000);
 }
 
@@ -270,47 +227,25 @@ function highScores() {
     console.log(originalContent);
     originalContent.hidden = true;
     highScoresPage.hidden = false;
-
     displayHighScoresOutput();
-
-
 }
 
 function saveHighScores() {
 
     var names = document.querySelector("#enterName");
 
-
     scores.push({ name: names.value, score: score });
     console.log(scores + " Scores adrer push")
-    // console.log(scores);
-    // var highScoreNames = {
-    //     name: names.value,
-    //     finalScore: score.value,
-    // };
-    // console.log(highScores.finalScore);
-    // console.log(highScoreNames.name + " this is high score names log in high score func");
-    // var personObj = new personHighScoreObj(names, score);
-    // console.log(personObj + " This is person obj in high score");
-    // localStorage.setItem("name", JSON.stringify(names.value))=ok
     var scoresStringed = JSON.stringify(scores);
-    // console.log(scoresStringed + " scores stringified");
     localStorage.setItem("item", scoresStringed);
-    // var user = JSON.parse(localStorage.getItem("item"));
-    // console.log(user, " this is the user after parse");
-    // localStorage.setItem(JSON.stringify(scores));
-    displayHighScoresOutput();
-
 }
 
 function displayHighScoresOutput() {
     var highScoresPage = document.querySelector(".displayParaHighScore");
-
     console.log(scores);
     scores.sort(function (b, a) {
         return a.score - b.score
     })
-
     console.log(scores);
     for (var i = 0; i < scores.length; i++) {
         // answerElement0.textContent = answerarrayq[numberOfQuestion].answers[0].text;
@@ -319,18 +254,20 @@ function displayHighScoresOutput() {
         var locscore = scores[i].score;
         console.log(locscore);
         console.log(locname + " Score: " + locscore);
-
-        var p2 = ("<p>" + locname + "Score: " + locscore + "</p>");
+        var p2 = ("<p>" + locname + " Score: " + locscore + "</p>");
         document.querySelector(".displayParaHighScore").insertAdjacentHTML('beforebegin', p2);
-
     }
-
+    gobackbt.hidden = false;
     // highScoresPage.textContent = user;
     // console.log(user + " this is the user entered and pulled");
-
 }
-
 function clearHighScores() {
     localStorage.clear();
     location.reload();
+    // highScores();
+}
+function showHighScore() {
+    displayHighScoresOutput();
+    highScoresPage.hidden = false;
+    gobackbt.hidden = true;
 }
